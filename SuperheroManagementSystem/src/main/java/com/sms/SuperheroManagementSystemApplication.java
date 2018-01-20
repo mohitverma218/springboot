d01c4d9500618c81d61f0d2145e4bd3c08298458
mohit <mohitverma218@gmail.com>
2018-01-21 00:15:08 +0100

    initial commit

---
+++ SuperheroManagementSystem/src/main/java/com/sms/SuperheroManagementSystemApplication.java
@@ -0,0 +1,12 @@
+package com.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperheroManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperheroManagementSystemApplication.class, args);
+	}
+}
