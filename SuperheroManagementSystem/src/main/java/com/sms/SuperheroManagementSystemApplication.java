7121c3f28d83d6fdc50cbc739b7e9a91cce3d0ab
mohit <mohitverma218@gmail.com>
2018-01-21 22:58:38 +0100

    second phase commit

--- SuperheroManagementSystem/src/main/java/com/sms/SuperheroManagementSystemApplication.java
+++ SuperheroManagementSystem/src/main/java/com/sms/SuperheroManagementSystemApplication.java
@@ -2,8 +2,10 @@ package com.sms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SuperheroManagementSystemApplication {
 
 	public static void main(String[] args) {
